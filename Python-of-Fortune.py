7507dd1c2b9bd7d55404ddf1736e87ef1e7c7a3e
William G <wgillihan@csumb.edu>
2015-12-07 00:10:39 -0800

    Create Python-of-Fortune.py

---
+++ Python-of-Fortune.py
@@ -0,0 +1,9 @@
+###### Team 4 Final Project ######
+#      William Gillihan          #
+#      Christopher Dixon         #
+#      Wendy Gray                #
+#      Brian Begun               #
+#                                #
+#      ver 12.07.15              #
+#      12_07_2015                #
+##################################
