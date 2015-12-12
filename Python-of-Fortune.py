2fe094d9b1bbf6b958385bcc4990b43e4e2e0a87
begunfx <begunfx@usa.net>
2015-12-12 09:23:54 -0800

    forgot to add import os

--- Python-of-Fortune.py
+++ Python-of-Fortune.py
@@ -14,6 +14,7 @@
 import random
 import copy
 import java.awt.Font as Font
+import os
 
 ###########################################################################  
 #################### BEGIN GAME SOUNDS AND WHEEL SPIN #####################
